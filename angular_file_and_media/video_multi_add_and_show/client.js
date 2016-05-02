0e1e078db4454369b97b74fdc4c1c35508e2f7c2
Eason_Xu <eason820720@163.com>
2016-05-02 16:57:11 +0800

    practice

---
+++ angular_file_and_media/video_multi_add_and_show/client.js
@@ -0,0 +1,12 @@
+import $ from 'jquery'
+import angular from 'angular'
+
+import 'angular-sanitize'
+import 'ng-file-upload'
+
+angular.module('app',['ngSanitize','ngFileUpload'])
+    .controller('MainController',['$scope',function($scope){
+
+    }])
+
+
