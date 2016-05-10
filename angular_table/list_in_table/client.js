cdf407a20bcc18dc8edc36595fac2e6b3d110424
easn xu <eason820720@gmail.com>
2016-05-10 17:47:07 +0800

    ..

--- angular_table/list_in_table/client.js
+++ angular_table/list_in_table/client.js
@@ -3,20 +3,28 @@ import angular from 'angular'
 
 import 'angular-sanitize'
 
-angular.module('app',['ngSanitize'])
-    .controller('MainController',['$scope',function($scope){
-           $scope.rows = [
-               {name: 'Meeting Room I', id: '1',  type:'room', task: [ { title: 'clean 1' }, {title: 'clean 2'}]},
-               {name: 'Meeting Room II', id: '2',  type:'room', task: [ { title: 'check 1' }, {title: 'check 2'}]},
-               {name: 'Rest Room I', id: '3',  type:'room', task: [ { title: 'check 1' }]},
-               {name: 'Rest Room II', id: '3',  type:'room', task: [ { title: 'check 1' }, {title: 'clean 2'}]}
-           ]
+angular.module('app', ['ngSanitize'])
+    .controller('MainController', ['$scope', function ($scope) {
+        $scope.rows = [
+            {name: 'Meeting Room I', id: '1', type: 'room', task: [{title: 'clean 1'}, {title: 'clean 2'}]},
+            {name: 'Meeting Room II', id: '2', type: 'room', task: [{title: 'check 1'}, {title: 'check 2'}]},
+            {name: 'Rest Room I', id: '3', type: 'room', task: [{title: 'check 1'}]},
+            {name: 'Rest Room II', id: '3', type: 'room', task: [{title: 'check 1'}, {title: 'clean 2'}]}
+        ]
 
-           $scope.headers = Object.keys($scope.rows[0]);
+        $scope.headers = Object.keys($scope.rows[0]);
+        $scope.action2 = false;
+        $scope.isTask = (item)=> {
+            return Array.isArray(item);
+        }
 
-          $scope.isTask= (item)=> {
-              return Array.isArray(item);
-          }
+        $scope.toggleHeader = ()=>{
+            $scope.headerAction = !$scope.headerAction;
+        }
+
+        $scope.toggleHeader2 = ()=>{
+            $scope.action2 = !$scope.action2;
+        }
     }])
 
 
