2fed79a5460e0f1a4d0a99917da035bd8984d2d0
Eason_Xu <eason820720@163.com>
2016-07-12 22:06:44 +0800

    update

--- angular_project/TextCollection/module/category/category.controller.js
+++ angular_project/TextCollection/module/category/category.controller.js
@@ -1,3 +1,39 @@
+// class CategoryController {
+//     constructor($scope, contextListService, contextService) {
+//         this.$scope = $scope;
+//         this.$scope.types = [
+//             {name: 'start', checked: false},
+//             {name: 'high', checked: false},
+//             {name: 'end', checked: false}
+//         ];
+//         $scope.items = [];
+//         this.contextListService = contextListService;
+//         this.init();
+//     }
+//
+//     init() {
+//         let self = this;
+//         this.contextListService.getContext()
+//             .then(()=> {
+//                 console.log(this);
+//                 this.$scope.items = this.contextListService.contextSavedList;
+//             })
+//     }
+//
+//     set(types) {
+//         let self = this;
+//         this.contextListService.setType(types);
+//         this.contextListService.saveTotalContext()
+//             .then(()=> {
+//                 self.init();
+//             });
+//     }
+// }
+//
+// CategoryController.$inject = ['$scope', 'contextListService', 'contextService'];
+//
+// export {CategoryController}
+
 angular.module('category.module')
     .controller('CategoryController', ['$scope', 'contextListService', 'contextService', '$timeout',
         function ($scope, contextListService, contextService, $timeout) {
--- angular_project/TextCollection/module/category/category.module.js
+++ angular_project/TextCollection/module/category/category.module.js
@@ -2,4 +2,5 @@ angular.module('category.module',
     [
         'ui.router'
     ])
+
   
