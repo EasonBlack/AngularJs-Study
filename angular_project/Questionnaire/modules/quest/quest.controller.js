adfbbbe300e50e2b99933ba710174afbab739136
Eason_Xu <eason820720@163.com>
2016-07-25 22:54:56 +0800

    update

--- angular_project/Questionnaire/modules/quest/quest.controller.js
+++ angular_project/Questionnaire/modules/quest/quest.controller.js
@@ -2,15 +2,9 @@ class QuestController {
     constructor() {
         this.message = 'Hello';
         this.quests = [
-            'aaaaa',
-            'bbbbb',
-            'ccccc',
-            'ddddd',
-            'eeeeee',
-            'fffff',
-            'bbbbbaas',
-            '1232323',
-            'ccxzzzz',
+            {type: 'radio', title: 'aaaaaaaaaaaa', items: ['aa','bbb'] },
+            {type: 'textarea', title: 'bbbbbbbbbbbb' },
+            {type: 'radio', title: 'ccccccccc', items: ['ee','wwww'] },
         ]
         this.current = 0;
     }
@@ -23,6 +17,26 @@ class QuestController {
     next() {
         this.current = this.current + 1;
         this.position = this.current * 100;
+    }
+
+    isFirst() {
+        if (this.current == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    isLast() {
+        if (this.current == this.quests.length - 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    confirm() {
+        alert(123);
     }
 
 }
