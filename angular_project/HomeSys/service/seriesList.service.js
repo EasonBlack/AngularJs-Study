2d41fb5bc0165826862d033a1f1bf4c80bf1e9e6
Eason_Xu <eason820720@163.com>
2016-08-24 23:52:24 +0800

    s

--- angular_project/HomeSys/service/seriesList.service.js
+++ angular_project/HomeSys/service/seriesList.service.js
@@ -1,25 +1,27 @@
 angular.module('app')
-    .factory('seriesListService', ['$http',function ($http) {
+    .factory('seriesListService', ['$http', function ($http) {
         var SeriesList = function (id, name) {
             var self = this;
             var headers = [
-                {id:1, name: 'name', title: 'NAME', type: 'href', href: '#/series/detail/'},
-                {id:2 , name: 'date', title: 'DATE'},
-                {id:3, name: 'status', title: 'STATUS'}
+                {id: 1, name: 'name', title: 'NAME', type: 'href', href: '#/series/detail/'},
+                {id: 2, name: 'date', title: 'DATE'},
+                {id: 3, name: 'status', title: 'STATUS'}
             ]
 
             this.initialize = function () {
                 $http.get('http://localhost:2003/series')
                     .then((res)=> {
-                        console.log(res.data);
-                        self.rows = res.data;
+                        self.rows = _.sortBy(res.data, function (d) {
+                            return -1 * d._id
+                        });
+                        console.log(self.rows);
                         self.headers = headers;
                     })
             };
 
-            this.add = function(name){
+            this.add = function (name) {
                 var obj = {}
-                if(!name) return ;
+                if (!name) return;
                 obj.name = name;
                 console.log('add series ' + name);
                 $http.post('http://localhost:2003/series', obj)
@@ -28,7 +30,7 @@ angular.module('app')
                     })
             }
 
-            this.addItem = function(id,item,cb) {
+            this.addItem = function (id, item, cb) {
                 $http.post('http://localhost:2003/series/' + id, item)
                     .then((res)=> {
                         alert('Success');
